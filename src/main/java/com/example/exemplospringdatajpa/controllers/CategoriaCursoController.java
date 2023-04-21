8c9ea8a5b14051d056d1a067bc2881ddc2fc1894
guavila31 <gustavoavila2000@hotmail.com>
2023-04-20 21:52:02 -0300

    Aula 2 -II

---
+++ src/main/java/com/example/exemplospringdatajpa/controllers/CategoriaCursoController.java
@@ -0,0 +1,26 @@
+package com.example.exemplospringdatajpa.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.exemplospringdatajpa.dtos.CategoriaCursoDTO;
+import com.example.exemplospringdatajpa.services.CategoriaCursoService;
+
+@RestController
+@RequestMapping("/api/categoriaCurso")
+public class CategoriaCursoController {
+    private CategoriaCursoService categoriaCursoService;
+
+    public CategoriaCursoController(CategoriaCursoService categoriaCursoService) {
+        this.categoriaCursoService = categoriaCursoService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Long inserir(CategoriaCursoDTO dto) {
+        return categoriaCursoService.inserir(dto);
+    }
+}
